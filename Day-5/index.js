c73f87bb1c8467e4b5f338eb657dcd07dd381dfe
fabcodingzest <fabrizvi786@gmail.com>
2023-01-05 01:09:49 +0530

    solved day 5 problems

---
+++ Day-5/index.js
@@ -0,0 +1,70 @@
+// To find a first pair from a number array whose sum is zero
+function findFirstPair(arr) {
+  let pairs = [];
+  arr.forEach((item1, i) => {
+    let pairArr =
+      i === 0
+        ? arr.slice(1)
+        : i === arr.length - 1
+        ? arr.slice(0, arr.length - 2)
+        : [...arr.slice(0, i), ...arr.slice(i + 1)];
+
+    // console.log(pairArr);
+    pairArr.forEach((item2) => {
+      if (item1 + item2 === 0) {
+        // console.log(item1, item2 + "=" + (item1 + item2));
+        pairs.push([item1, item2]);
+      }
+    });
+  });
+  return pairs[0];
+}
+// console.log(findFirstPair([1, 2, 4, -2, -1, 6]));
+
+// To find a first pair from a number array whose sum is zero using indexing
+function findFirstPair2(arr) {
+  let pairs = [];
+  for (let i = 0; i < arr.length; i++) {
+    let pairArr =
+      i === 0
+        ? arr.slice(1)
+        : i === arr.length - 1
+        ? arr.slice(0, arr.length - 2)
+        : [...arr.slice(0, i), ...arr.slice(i + 1)];
+
+    // console.log(pairArr);
+    for (let j = 0; j < pairArr.length; j++) {
+      if (arr[i] + pairArr[j] === 0) {
+        // console.log(arr[i], pairArr[j] + "=" + (arr[i] + pairArr[j]));
+        pairs.push([arr[i], pairArr[j]]);
+      }
+    }
+  }
+  return pairs[0];
+}
+console.log(findFirstPair2([1, 2, 4, -2, -1, 6]));
+
+// To find the largest pair of the 2 elements using indexing with unsorted elements
+function findLargestPair(arr) {
+  let pair = [];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let pairArr =
+      i === 0
+        ? arr.slice(1)
+        : i === arr.length - 1
+        ? arr.slice(0, arr.length - 2)
+        : [...arr.slice(0, i), ...arr.slice(i + 1)];
+
+    // console.log(pairArr);
+    for (let j = 0; j < pairArr.length; j++) {
+      if (arr[i] + pairArr[j] > sum) {
+        // console.log(arr[i], pairArr[j] + "=" + (arr[i] + pairArr[j]));
+        sum = arr[i] + pairArr[j];
+        pair = [arr[i], pairArr[j]];
+      }
+    }
+  }
+  return { pair, sum };
+}
+console.log(findLargestPair([1, 2, 4, -2, -1, 6]));
